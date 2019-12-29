7bd6b2bb71693c5f187a3fff51723fd14183f3be
PrimeWalkerVN <nchithanh609@gmail.com>
2019-12-29 12:51:34 +0700

    multiselected+time

--- MultiMediaPlayer/MainWindow.xaml.cs
+++ MultiMediaPlayer/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MultiMediaPlayer
 {
@@ -27,15 +28,40 @@ namespace MultiMediaPlayer
         MediaPlayer _player = new MediaPlayer();
         public bool _isPlaying = false;
         BindingList<FileInfo> _fullPaths = new BindingList<FileInfo>();
+        DispatcherTimer _timer;
+
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += timer_Tick;
+            _timer.Start();
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
            PlayList.ItemsSource = _fullPaths;
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (_player.Source != null)
+            {
+                
+                var currentPos = _player.Position.ToString(@"mm\:ss");
+                var duration = _player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+
+                Title = String.Format($"{currentPos} / {duration}");
+                CurrentTime.Text = currentPos;
+                DurationTime.Text = duration;
+            }
+            else
+                Title = "No file selected...";
         }
 
         private void PlayList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -51,17 +77,25 @@ namespace MultiMediaPlayer
                 "*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;" +
                 "*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;" +
                 "*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";
+            op.Multiselect = true;
 
             if (op.ShowDialog() == true)
             {
-                String fileName;
-                fileName = op.FileName; 
-                MessageBox.Show($"Load successful! ....{fileName}\n");
-                _player.Open(new Uri(fileName, UriKind.Absolute));
+                String[] fileNames;
+                fileNames = op.FileNames;
 
                 //Add to array to display into listview
-                var info = new FileInfo(op.FileName);
-                _fullPaths.Add(info);
+                for (int i = 0; i < fileNames.Length; i++)
+                {
+                    var info = new FileInfo(fileNames[i]);
+                    _fullPaths.Add(info);
+                }
+                
+                MessageBox.Show($"Load successful! ....\n");
+                _player.Open(new Uri(fileNames[0], UriKind.Absolute));
+
+                
+               
                 return;
             }
             else
